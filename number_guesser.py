436466a87cae976c059d832a194cd8b3898da21c
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-05-08 13:18:53 +0100

    Updated number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -5,13 +5,17 @@ stop = 11
 
 top_of_range = input("Type a number: ")
 
-if top_of_range.isdigit():
+if top_of_range.lstrip('-').isdigit():
     top_of_range = int(top_of_range)
 
     if top_of_range <= start:
-        print ("Please write the number bigger than " + start + ".")
+        print ("Please write the number larger than ", start," next time.")
         quit()
 
+else :
+    print ("Please write a number next time.")
+    quit()
 
-random_number = random.randint(start, stop)
+random_number = random.randint(start, top_of_range)
+print("Random number between", start, "and", top_of_range, ":", random_number)
 
